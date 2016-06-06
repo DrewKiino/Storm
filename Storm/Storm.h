9b840373c2a8163e829845cc71d68f87428c1193
Andrew Aquino <andrewaquino118@yahoo.com>
2016-06-06 11:46:21 -0700

    dbg

---
+++ Storm/Storm.h
@@ -0,0 +1,19 @@
+//
+//  Storm.h
+//  Storm
+//
+//  Created by Andrew Aquino on 6/6/16.
+//  Copyright © 2016 Andrew Aquino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Storm.
+FOUNDATION_EXPORT double StormVersionNumber;
+
+//! Project version string for Storm.
+FOUNDATION_EXPORT const unsigned char StormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Storm/PublicHeader.h>
+
+
